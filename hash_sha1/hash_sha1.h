67e8736a41e3e068416a9e8a482a1342c0d62633
Iliass Mahjoub <48915588+imahjoub@users.noreply.github.com>
2022-10-11 23:18:54 +0200

    Update hash_sha1.h

--- hash_sha1/hash_sha1.h
+++ hash_sha1/hash_sha1.h
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////
+//                                                               //
+//  Copyright Iliass Mahjoub 2019 - 2022.                        //
+//  Distributed under the Boost Software License,                //
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt          //
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)             //
+//                                                               //
+///////////////////////////////////////////////////////////////////
+
 #ifndef HASH_SHA1_2019_11_30_H
   #define HASH_SHA1_2019_11_30_H
 
